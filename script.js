60f6db396b2bc2430b4908f24ae70508803a03c8
nursedaturkcan123@gmail.com <nursedaturkcan123@gmail.com>
2023-04-28 15:36:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function hamburgerMenu() {
+    var x = document.querySelector(".navigation-nav-list");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
